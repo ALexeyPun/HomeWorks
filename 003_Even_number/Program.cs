67c18b55a2a853a0fe4e38d10a3b864e9c26ac7c
AlexeyPanshin <dispes@mail.ru>
2022-12-16 01:30:51 +0300

    add even number

---
+++ 003_Even_number/Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.WriteLine("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int remainder = number % 2;
+
+if(remainder == 0)System.Console.WriteLine( number + " - is an even number.");
+else System.Console.WriteLine( number + " - is not an even number.");
