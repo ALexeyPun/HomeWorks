c1935ffc860528b046c8c10b2ac43b53995f08f1
AlexeyPanshin <dispes@mail.ru>
2022-12-16 02:02:48 +0300

    add enum

---
+++ 004_Enam/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Enter number: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if (userNumber < 2) System.Console.WriteLine("Your number does not match the format.");
+if (1 == userNumber % 2) userNumber--;
+
+for (int i = 2; i <= userNumber; i += 2)
+{
+    Console.WriteLine(i);
+}
