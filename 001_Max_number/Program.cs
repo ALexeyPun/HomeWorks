e7fc3c621369ceb0f2916549592a52d2816c125d
AlexeyPanshin <dispes@mail.ru>
2022-12-16 00:42:58 +0300

    add 001 max number

---
+++ 001_Max_number/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите первое число: ");
+int oneNumber = Convert.ToInt32(System.Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int twoNumber = Convert.ToInt32(System.Console.ReadLine());
+
+if (oneNumber > twoNumber)
+    System.Console.WriteLine("Число " + oneNumber + " больше числа " + twoNumber);
+else
+    System.Console.WriteLine("Число " + twoNumber + " больше числа " + oneNumber);
