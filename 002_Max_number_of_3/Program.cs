829008378c072d83feee4951bf8912cc2639cbd7
AlexeyPanshin <dispes@mail.ru>
2022-12-16 01:10:35 +0300

    add max number of three numbers

---
+++ 002_Max_number_of_3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Enter number one: ");
+int oneNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter number two: ");
+int twoNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter number three: ");
+int threeNumber = Convert.ToInt32(Console.ReadLine());
+
+int max = oneNumber;
+
+if (max < twoNumber) max = twoNumber;
+if (max < threeNumber) max = threeNumber;
+
+Console.WriteLine("Max number = " + max);
