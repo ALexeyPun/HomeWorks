8c007361d4b3e0a47cf70372bf43b7c9cce9edf0
AlexeyPanshin <dispes@mail.ru>
2022-12-23 07:06:55 +0300

    add palindrome

---
+++ 007_Palindrome/Program.cs
@@ -0,0 +1,47 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+int[] AssignmentToArray(int number, int size)
+{
+    int temporaryNumber = 0;
+    int[] array = new int[size--];
+
+    for (int i = 0; i <= size; i++)
+    {
+        temporaryNumber = number % 10;
+        number /= 10;
+        array[size - i] = temporaryNumber;
+    }
+    return array;
+}
+
+bool Examination(int[] array, int size)
+{
+    bool check = true;
+    for (int i = 0; i < size / 2; i++)
+    {
+        if (array[i] != array[size - 1 - i])
+        {
+            check = false;
+            break;
+        }
+    }
+    return check;
+}
+
+Console.Clear();
+System.Console.WriteLine("Alexey Panshin's homework!");
+System.Console.WriteLine("");
+
+System.Console.Write("Enter number: ");
+string userEnter = Console.ReadLine();
+
+int size = (userEnter.Length);
+int palindrome = Convert.ToInt32(userEnter);
+
+int[] array = new int[size];
+
+array = AssignmentToArray(palindrome, size);
+bool check = Examination(array, size);
+
+if(check) Console.WriteLine("The Number is a palindrome!");
+else Console.WriteLine("Number is not a palindrome!");
